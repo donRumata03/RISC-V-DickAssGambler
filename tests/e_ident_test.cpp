3f6b5f08933eaf0a4f583b663e71fa2ba9c1bc72
donRumata <donrumata03@gmail.com>
2021-12-11 21:15:23 +0300

    Improve ident parsing test

--- tests/e_ident_test.cpp
+++ tests/e_ident_test.cpp
@@ -10,13 +10,9 @@
 
 
 TEST(HeaderTests, E_IDENT) {
-	std::cout << test1_elf_path << std::endl;
-	std::cout << test2_elf_path << std::endl;
-
 	auto ident1 = ElfHeaderIdent::read_from_bytes(read_bytes(test1_elf_path));
 	auto ident2 = ElfHeaderIdent::read_from_bytes(read_bytes(test2_elf_path));
-//	auto ident_failure = ElfHeaderIdent::read_from_bytes(read_bytes(x64_elf_path));
 
-
+	EXPECT_THROW(ElfHeaderIdent::read_from_bytes(read_bytes(x64_elf_path)), std::runtime_error);
 }
 
