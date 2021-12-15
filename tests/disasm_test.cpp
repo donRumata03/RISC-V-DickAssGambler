1e6873270f4b8ca553f81161e0c684d92ad66a80
donRumata <donrumata03@gmail.com>
2021-12-15 19:17:46 +0300

    Add IImmediate tests

--- tests/disasm_test.cpp
+++ tests/disasm_test.cpp
@@ -11,10 +11,15 @@
 #include "elf_parsing/ElfFile.h"
 #include "workflow.h"
 #include "risc_v/rv32_parser.h"
+#include "risc_v/InstructionArgument.h"
 
 
 TEST(DisAsm, Test1Elf) {
 	print_disasm(test1_elf_path);
+}
+
+TEST(DisAsm, Test2Elf) {
+	print_disasm(test2_elf_path);
 }
 
 TEST(DisAsm, EcallCommand) {
@@ -27,5 +32,16 @@ TEST(DisAsm, BImmediate) {
 	// 1111000100001100011 --> beq	a5, zero, 10088
 	let instr = parse_RV32_instruction(0x00078863);
 
-	EXPECT_EQ(format_instruction(instr), "ecall");
+	EXPECT_EQ(std::get<i32>(*instr.immediate), 10088);
+}
+
+TEST(DisAsm, IImmediate) {
+	//    4fc50513          	addi	a0, a0, 1276
+	let instr_pos = parse_RV32_instruction(0x4fc50513);
+
+	//    dfc18193          	addi	gp, gp, -516
+	let instr_neg = parse_RV32_instruction(0xdfc18193);
+
+	EXPECT_EQ(std::get<i32>(*instr_pos.immediate), 1276);
+	EXPECT_EQ(std::get<i32>(*instr_neg.immediate), -516);
 }
